11af43e0e5c5986c1cdae03889aa28369dfe5df4
Patryk <patryk.piasecki1@gmail.com>
2023-01-18 20:46:53 +0100

    implemented smooth scrolling for the elements in the navigation bar

--- Bankist_WebPage/script.js
+++ Bankist_WebPage/script.js
@@ -12,39 +12,58 @@ const section1 = document.getElementById("section--1");
 
 
 const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
 
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+    btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
 });
 
 btnScrollTO.addEventListener('click', e => {
-/*  const s1coords = section1.getBoundingClientRect();
+    // LEGACY CODE
+    /*  const s1coords = section1.getBoundingClientRect();
 
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset, // Top relative to the viewport
-    behavior: 'smooth',
-  })*/ // LEGACY CODE
+      window.scrollTo({
+        left: s1coords.left + window.pageXOffset,
+        top: s1coords.top + window.pageYOffset, // Top relative to the viewport
+        behavior: 'smooth',
+      })*/
 
-section1.scrollIntoView({behavior: 'smooth'})
+    section1.scrollIntoView({behavior: 'smooth'})
 })
 
+// PAGE NAVIGATION
+
+// document.querySelectorAll('.nav__link').forEach(el => {
+//   el.addEventListener('click', e => {
+//     e.preventDefault();
+//
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+//   })
+// })
+
+document.querySelector('.nav__links').addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+    }
+})
