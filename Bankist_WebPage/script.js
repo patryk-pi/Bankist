b048be5b1107c6f2e750712e954905ec6c57de5a
Patryk <patryk.piasecki1@gmail.com>
2023-01-19 19:13:49 +0100

    implemented slider

--- Bankist_WebPage/script.js
+++ Bankist_WebPage/script.js
@@ -143,7 +143,7 @@ const allSections = document.querySelectorAll('.section');
 const revealSection = (entries, observer) => {
     const [entry] = entries;
 
-    if(!entry.isIntersecting) return;
+    if (!entry.isIntersecting) return;
 
     entry.target.classList.remove('section--hidden');
     observer.unobserve(entry.target);
@@ -185,3 +185,52 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+
+// SLIDER
+
+const slides = document.querySelectorAll('.slide');
+
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const slider = document.querySelector('.slider');
+slider.style.transform = 'scale(0.3)';
+slider.style.overflow = 'visible';
+
+
+const goToSlide = slide => {
+    slides.forEach((s, i) => {
+        s.style.transform = `translateX(${100 * (i - slide)}%)`
+    });
+};
+
+goToSlide(0);
+
+// Next Slide
+
+const nextSlide = () => {
+    if (curSlide === maxSlide - 1) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+    goToSlide(curSlide);
+};
+
+const prevSlide = () => {
+    if (curSlide === 0) {
+        curSlide = maxSlide - 1;
+    } else {
+        curSlide--;
+    }
+    goToSlide(curSlide)
+};
+
+
+btnRight.addEventListener('click', nextSlide);
+
+btnLeft.addEventListener('click', prevSlide);
